9b4bee1f8aff6077930c90a92218ae06e5a4477b
kelly cristina <kellycrisbarbosalima@gmail.com>
2022-09-16 16:54:42 -0300

    Operadores de incremento e decremento

---
+++ operadores_de_incremento.js
@@ -0,0 +1,17 @@
+
+alert('Código Funcionando!') // mostrar uma mensagem para o usuario      
+const nomeUsuario = prompt('Olá, usuário, qual o seu nome?') // mostra msg e recebe um valor
+
+let num = Number(prompt('Olá,' + nomeUsuario + ' Digite algum número, por favor'))
+//console.log(typeof num) // retorna o tipo da variavel armazena
+
+//const sucessor = num ++
+//const antecessor = num - 1
+
+//alert('O sucessor de ' + num + ' é ' + sucessor)
+alert((num + 1))
+
+
+
+
+
