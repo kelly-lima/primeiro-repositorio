b281b0bf551d0fd34b11d4ff9f7cae26311583bf
kelly cristina <kellycrisbarbosalima@gmail.com>
2022-09-15 22:03:25 -0300

    Primeiro arquivo JavaScript

---
+++ index.js
@@ -0,0 +1,18 @@
+// serve para escrever no terminal
+console.log("Hello word")
+console.log("7")
+console.log(154.23)
+console.log(true)
+console.log(false)
+
+//string -> texto
+// number -> números gerais
+// boolean -> true / false
+
+// variáveis
+
+let mensagem = "Estou aprendendo JavaScript"
+console.log(mensagem)
+
+mensagem = "Aprendi a mudar o valor da variável"
+console.log(mensagem)
