3164ae45204a3e3e02032d2a75cec120e917bb2a
kelly cristina <kellycrisbarbosalima@gmail.com>
2022-09-20 17:59:23 -0300

    Aula Strings  20/09/2022

---
+++ strings.js
@@ -0,0 +1,39 @@
+//const aspasSimples = 'olá, mundo'
+//console.log(aspasSimples)
+
+
+// \n -> quebra de linha
+//const aspasDuplas = "Olá eu sou o JavaScript. \n Eu sou melhor que o Python"
+//console.log(aspasDuplas)
+
+
+//const textoComCrase = `Olá eu sou 
+//o JavaScript. Eu sou melhor`
+//console.log(textoComCrase)
+
+//const nomeUsuario = prompt('Olá usuário, Qual o seu nome?')
+
+const texto = 'O JavaScript foi criado alguns anos após o Python. No entanto, o JavaScript é bem mais bacana '
+
+        console.log(texto)
+        console.log(texto.toUpperCase())
+        console.log(texto.toLocaleLowerCase())
+        console.log(texto.concat('A linguagem C é a mãe de todas as Linguagens'))
+        console.log(texto.substring(2))
+
+        console.log(texto.replace('JavaScript', 'Rust'))
+        console.log(texto.replaceAll('JavaScript', 'Kotlin'))
+        console.log(texto.replace(/JavaScript/g, 'PHP'))
+        console.log(texto.replace(/JavaScript/gi, 'PHP'))
+
+        const html = '<!DOCTYPE html>\n<html><html/>'
+
+        console.log(html.startsWith('<!DOCTYPE html'))
+        console.log(html.startsWith('</bordy'))
+
+        
+
+    
+
+        
+        
