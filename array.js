6329f4160c9437b98e4e9cdc3d556a0d8fee0b33
kelly cristina <kellycrisbarbosalima@gmail.com>
2022-09-23 16:56:34 -0300

    Arrays

---
+++ array.js
@@ -0,0 +1,17 @@
+//let arr = new Array()
+//let arr2 = new Array(5)  5 campos vazio
+//undefined = significa não existe
+let arr3 = [10, 5, 6, 9, 7,'Eu sou um texto dentro de um array', 9.8 , true, 'morango']
+
+console.log(arr3)
+console.log(arr3[5], arr3[8])
+
+arr3[0] = 'batata'
+console.log(arr3[0])
+
+arr3[4] = 10*10
+console.log(arr3)
+
+arr3[8] = 'Agora a posição 8 existe'
+
+console.log(arr3)
--- desafio.js
+++ desafio.js
@@ -1,61 +1,53 @@
- /* 
- Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço
-normal de etiqueta e a escolha da condição de pagamento. Utilize os códigos da tabela a seguir
-para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado.
-Código Condição de pagamento
 
-1 À vista em dinheiro ou cheque, recebe 10% de desconto
-2 À vista no cartão de crédito, recebe 15% de desconto
-3 Em duas vezes, preço normal de etiqueta sem juros
-4 Em três vezes, preço normal de etiqueta mais juros de 10%
- */
-
-/*
-Método de pagamento
-Em dinheiro
-Em cheque
-No crtão
-*/
-
-//Recebendo o preco do produto e transformando o valor para decimal
-// analise feita do conteúdo não do seu tipo por isso aceita string
-const precoDoProduto = parseFloat(prompt('Qual o preço do produto?'))
-const mensagem = `
+// recebendo o preço do produto e transformando o valor para decimal
+let precoProduto = parseFloat(prompt('Qual o preço do produto?')) // 700 Nan
+while(isNaN(precoProduto) == true ) {
+    alert('O preço digitado é inválido. Por favor, informe o preço apenas com números, sem carácteres especias (letras ou acentos)')
+    precoProduto = parseFloat(prompt('Qual o preço do produto?')) // 700 Nan
+}
+const mensagem = 
+`
 Informe o método de pagamento:
 Digite 1 para pagamento em dinheiro
 Digite 2 para pagamento em cheque
 Digite 3 para pagamento com cartão
 `
-const metodoDePagamento = prompt(mensagem)
+const metodoDePagamento = prompt(mensagem) // 2
 
-console.log (precoProduto)
+console.log(precoProduto)
 console.log(metodoDePagamento)
 
+/**
+ * Se o valor da constante 'metodoDePagamento' for 1 ou 2, significa que a pessoa
+ * possuirá um desconto de 10%
+ */
 
-if (metodoDePagamento == 1 || metodoDePagamento == 2) {
-    const precoFinal = precoProduto * 0.90
-    alert(`O preço final de pagamento é ${precoFinal.toFixed(2)} reais`)
-} else if (metodoDePagamento == 3) {
-
-    /**
-     * 1 parcela = 15% de desconto
-     * 2 parcelas = preço do produto
-     * 3 ou mais parcelas = acrescimo de 10%
-     */
-    const parcelas = parseInt(prompt("Informe a quantidade de parcelas desejadas:"))
-
-    if(parcelas==1){
-        const precoFinal = precoProduto * 0.85
-        alert(`O preço final de pagamento é ${precoFinal.toFixed(2)} reais`)
-    } else if(parcelas==2){
-        const precoFinal = precoProduto
-        alert(`O preço final de pagamento é ${precoFinal.toFixed(2)} reais`)
-    } else {
-        const precoFinal = precoProduto * 1.10
-        alert(`O preço final de pagamento é ${precoFinal.toFixed(2)} reais`)
-    }
+ while (metodoDePagamento < 1 || metodoDePagamento > 3) {
+    alert('Método de pagamento inválido. Selecione novamente')
+    metodoDePagamento = parseInt(prompt(mensagem))
 }
 
 
 
-
+ if (metodoDePagamento == 1 || metodoDePagamento == 2) {
+    const precoFinal = precoProduto * 0.9
+    alert(`O preço final do pagamento é ${precoFinal} reais`)
+  } else if (metodoDePagamento == 3) {
+    /**
+     * 1 parcela = 15% de desconto
+     * 2 parcelas = preço do produto
+     * 3 ou mais parcelas = acréscimo de 10%
+     */
+    
+    const parcelas = parseInt(prompt('Informe a quantidade de parcelas'))
+  
+    if (parcelas == 1) {
+      const precoFinal = precoProduto * 0.85
+      alert(`O preço final do pagamento é ${precoFinal} reais`)
+    } else if (parcelas == 2) {
+      alert(`O preço final do pagamento é ${precoProduto} reais`)
+    } else {
+      const precoFinal = precoProduto * 1.1
+      alert(`O preço final do pagamento é ${precoFinal} reais`)
+    }
+}
